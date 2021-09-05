7bae2c53c8746a36b3af279af99024776e79df38
unemekenta <unemekenta@gmail.com>
2021-09-05 12:08:27 +0900

    add vue

---
+++ frontend/src/atoms/Pager.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  paginate(
+    v-model="currentPage"
+    :page-count="maxPage"
+    :page-range="3"
+    :margin-pages="2"
+    :click-handler="clickCallback"
+    :prev-text="'<'"
+    :next-text="'>'"
+    :container-class="'pagination'"
+    :page-class="'pagination-item'"
+  )
+</template>
+
+<script>
+import Paginate from 'vuejs-paginate'
+
+export default {
+  name: 'Pager',
+  components: {
+    Paginate,
+  },
+  methods: {
+    clickCallback (pageNum) {
+      this.currentPage = Number(pageNum);
+      this.$emit('changePage', pageNum);
+    }
+  },
+  data () {
+    return {
+      currentPage: 1,
+    }
+  },
+  props: {
+    maxPage: Number,
+  },
+}
+</script> 
+
+<style lang="scss" src="../assets/css/pager.scss">
+// pagerはscopedが効かないため記述しない
+</style>
