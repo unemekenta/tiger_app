3570eb798532ca48269f1ab46b824510cf8d4914
unemekenta <unemekenta@gmail.com>
2021-09-12 13:32:49 +0900

    goファイルの追加

--- backend_src/handlers/websites.go
+++ backend_src/handlers/websites.go
@@ -22,6 +22,41 @@ func GetWebsites(c echo.Context) error {
 	return c.JSON(http.StatusOK, websites)
 }
 
+func GetWebsitesByCategory(c echo.Context) error {
+	var websites []models.Websites
+	Db := models.OpenDBConn()
+	db, err := Db.DB()
+	defer db.Close()
+
+	category_id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return err
+	}
+
+	type Results struct {
+		ID int
+	}
+	var results []Results
+	Db.Table("categories_websites").Where("category_id = ?", category_id).Scan(&results)
+	if err != nil {
+		return err
+	} else if len(results) == 0 {
+		return c.JSON(http.StatusOK, websites)
+	}
+
+	var websites_by_category_ids []int
+
+	for i := 0; i < len(results); i++ {
+		websites_by_category_ids = append(websites_by_category_ids, results[i].ID)
+	}
+
+	Db.Where(websites_by_category_ids).Find(&websites)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, websites)
+}
+
 func GetWebsite(c echo.Context) error {
 	website := new(models.Websites)
 	Db := models.OpenDBConn()
