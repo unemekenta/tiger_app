20d66e3d8ca8be44076fa3e59f5ea1a94ac68aab
unemekenta <unemekenta@gmail.com>
2022-01-01 00:42:38 +0900

    fix go

--- backend/models/redis.go
+++ backend/models/redis.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"crypto/tls"
 	"fmt"
 	"os"
 
@@ -10,10 +11,17 @@ import (
 func OpenRedisConn() *redis.Client {
 	addr := fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
 	pass := fmt.Sprintf("%s", os.Getenv("REDIS_PASS"))
+	insecureSkipVerify := true
+	if os.Getenv("MODE") == "local" {
+		insecureSkipVerify = false
+	}
 	redis := redis.NewClient(&redis.Options{
 		Addr:     addr,
 		Password: pass,
 		DB:       0,
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: insecureSkipVerify,
+		},
 	})
 	return redis
 }
