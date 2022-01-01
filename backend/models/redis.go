de73661e1618b466dbd1edb28065208c8aeb86a5
unemekenta <unemekenta@gmail.com>
2022-01-02 03:29:55 +0900

    fix

--- backend/models/redis.go
+++ backend/models/redis.go
@@ -3,20 +3,22 @@ package models
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
 
 func RedisNewPool() *redis.Pool {
-	redisUrl := fmt.Sprintf("%s://%s@%s:%s", os.Getenv("REDIS_TLS"), os.Getenv("REDIS_PASS"), os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
+	redisUrl := fmt.Sprintf("redis://%s@%s:%s", os.Getenv("REDIS_PASS"), os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
+	tsl, _ := strconv.ParseBool(os.Getenv("REDIS_PASS"))
 
 	return &redis.Pool{
 		MaxIdle:     3,
 		MaxActive:   0,
 		IdleTimeout: 120 * time.Second,
 		Dial: func() (redis.Conn, error) {
-			c, err := redis.DialURL(redisUrl)
+			c, err := redis.DialURL(redisUrl, redis.DialTLSSkipVerify(tsl))
 			if err != nil {
 				return nil, err
 			}
